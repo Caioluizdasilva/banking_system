f95731b23c8485bdefb268429626aac70f203e2e
Caio Luiz da Silva <caio.silva.3@aluno.cefet-rj.br>
2025-07-30 16:02:46 -0300

    add_newfiles

---
+++ banking.py
@@ -0,0 +1,24 @@
+def main();
+    while True;
+        print("\n --- Banking System")
+        print("1. Deposit")
+        print("2. Withdraw")
+        print("3. Check Balance")
+        print("4. Exit")
+
+       choice = input("Choose an option: ")
+    
+      if choice == '1';
+     amount =  float(input("Enter amout to deposit: $"))
+    # deposit funds
+
+    elif choice == '2';
+    amount = float(input("Enter amount to withdraw: $"))
+    #withdraw funds
+
+    elif choice =='3';
+#
+
+
+
+    
